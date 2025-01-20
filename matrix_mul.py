3dd4f049453c9d7f123d1cc265138c9abc08594e
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2025-01-20 20:02:15 +0000

    Creating the Vector Method

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,24 +1,32 @@
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 
 # Load data
-data = pd.read_csv('path/to/data')
+#data = pd.read_csv('path/to/data')
 
 # Example arrays
-Prices = [[300, 500],
+#Prices = [[300, 500], 2x2
           [1000, 120.85]]
 
-Array2 = [200, 100]
+#Array2 = [[200], [100]] 2x1 
 
 # Calculate the result
 Ans = []
 # (300*200 + 500*100) as an example calculation
 
+# This is a way to return a vector from multi-dimensional array multiplication 
+# This is one of 2 results of the matrix multiplication 
+# If the Array2 is a simple array , it will be treated as a simple vector 
+
+var = [200,100]
+new_arr = []
+# We start looping from the start of the price indices and initialise a row_sum variable which will reset to 0 every iteration
 for i in range(len(Prices)):
     row_sum = 0
-    for j in range(len(Prices[0])):
-        # COMPLETE THE MISSING LOGIC HERE
-        pass
-
-
-print(Ans)
+    #Since prices is a multidimensional list , they first element is also going to be a list and we are going to loop over the range of its size  
+    for j in range(len(Prices[i])):  
+        # So we multiply the prices row by the array2 column , since j only changes and i changes after j's iteration is done        
+        row_sum += Prices[i][j] * var[j] 
+        # We append the sum to the list as a normal result 
+    new_arr.append(row_sum) 
+print(new_arr)
